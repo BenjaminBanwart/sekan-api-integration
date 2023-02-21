1282b7adfb15c9114ef4527e3204adea497afbab
Benjamin Banwart <benjaminbanwart@Benjamins-MacBook-Pro.local>
2023-02-21 13:00:34 -0600

    updated controller

--- controllers/orderhive_get_products_controller.js
+++ controllers/orderhive_get_products_controller.js
@@ -3,24 +3,15 @@ require('dotenv').config();
 var axios = require('axios');
 var aws4  = require('aws4')
 
-var access_key_id
-var secret_key
-var session_token
-
-// Date for orderhive query
-var now = Date.now();
-var then = Date.now() - 28800;
-
-// router.get('/', async (req, res) => {
-
-// })
-
-
 
 router.post('/', async (req, res) => {
-    access_key_id = req.body.access_key_id
-    secret_key = req.body.secret_key
-    session_token = req.body.session_token
+    var access_key_id = req.body.access_key_id
+    var secret_key = req.body.secret_key
+    var session_token = req.body.session_token
+
+    // Date for orderhive query where I need timestamp for 8 hours before
+    var now = Date.now();
+    var then = Date.now() - 28800;
 
     // get the various date formats needed to form our request
     var amzDate = getAmzDate(new Date().toISOString());
@@ -61,31 +52,26 @@ router.post('/', async (req, res) => {
         // }
     }
     
+    //Sign the request
     var signedString = aws4.sign( options, {
         secretAccessKey: secret_key,
         accessKeyId: access_key_id,
         sessionToken: session_token,
     }).headers.Authorization
 
-    
 
-    const data = {'success': true};
     //Configure our call to get new products from Orderhive
     var getProductConfig = {
-    // port: 443,
     host: 'https://api.orderhive.com',
     method: 'POST',
     url: 'https://api.orderhive.com/product/listing/flat?page=1',
-    // body: JSON.stringify(data),
-    // data,
     path: "/product/listing/flat",
     headers: {
         'id_token': `${access_key_id}`,
         'X-Amz-Security-Token': `${session_token}`,
         'secretAccessKey': `${secret_key}`,
-        'X-Amz-Date': `${amzDate}`,
         'Authorization': `${signedString}`,
-        'X-Amz-Date': amzDate,
+        'X-Amz-Date': `${amzDate}`,
         'Content-Type': 'application/json'
     },
     data: {
